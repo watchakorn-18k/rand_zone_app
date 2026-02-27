334ef7dc889551353561d869ec56c7164e8c6121
watchakorn-18k <wk18k@proton.me>
2026-02-28 04:50:10 +0700

    update google-fonts.ts

--- src/lib/data/google-fonts.ts
+++ src/lib/data/google-fonts.ts
@@ -1,112 +1,119 @@
-export const ALL_GOOGLE_FONTS = [
-  {
-    "name": "Anakotmai",
-    "isThai": true,
-    "isLocal": true
-  },
-  {
-    "name": "Athiti",
-    "isThai": true
-  },
-  {
-    "name": "Bai Jamjuree",
-    "isThai": true
-  },
-  {
-    "name": "Boon",
-    "isThai": true,
-    "isLocal": true
-  },
-  {
-    "name": "Chakra Petch",
-    "isThai": true
-  },
-  {
-    "name": "Charm",
-    "isThai": true
-  },
-  {
-    "name": "Charmonman",
-    "isThai": true
-  },
-  {
-    "name": "Chonburi",
-    "isThai": true
-  },
-  {
-    "name": "Fahkwang",
-    "isThai": true
-  },
-  {
-    "name": "IBM Plex Sans Thai",
-    "isThai": true
-  },
-  {
-    "name": "IBM Plex Sans Thai Looped",
-    "isThai": true
-  },
-  {
-    "name": "Itim",
-    "isThai": true
-  },
-  {
-    "name": "Kanit",
-    "isThai": true
-  },
-  {
-    "name": "Kodchasan",
-    "isThai": true
-  },
-  {
-    "name": "KoHo",
-    "isThai": true
-  },
-  {
-    "name": "Krub",
-    "isThai": true
-  },
-  {
-    "name": "Kufam",
-    "isThai": true
-  },
-  {
-    "name": "Mali",
-    "isThai": true
-  },
-  {
-    "name": "Mitr",
-    "isThai": true
-  },
-  {
-    "name": "Niramit",
-    "isThai": true
-  },
-  {
-    "name": "Pattaya",
-    "isThai": true
-  },
-  {
-    "name": "Pridi",
-    "isThai": true
-  },
-  {
-    "name": "Prompt",
-    "isThai": true
-  },
-  {
-    "name": "Sarabun",
-    "isThai": true
-  },
-  {
-    "name": "Srisakdi",
-    "isThai": true
-  },
-  {
-    "name": "Taviraj",
-    "isThai": true
-  },
-  {
-    "name": "Trirong",
-    "isThai": true
-  }
+export interface GoogleFont {
+	name: string;
+	isThai: boolean;
+	isLocal?: boolean;
+	cssUrl?: string;
+}
+
+export const ALL_GOOGLE_FONTS: GoogleFont[] = [
+	{
+		name: 'Anakotmai',
+		isThai: true,
+		isLocal: true
+	},
+	{
+		name: 'Athiti',
+		isThai: true
+	},
+	{
+		name: 'Bai Jamjuree',
+		isThai: true
+	},
+	{
+		name: 'Boon',
+		isThai: true,
+		isLocal: true
+	},
+	{
+		name: 'Chakra Petch',
+		isThai: true
+	},
+	{
+		name: 'Charm',
+		isThai: true
+	},
+	{
+		name: 'Charmonman',
+		isThai: true
+	},
+	{
+		name: 'Chonburi',
+		isThai: true
+	},
+	{
+		name: 'Fahkwang',
+		isThai: true
+	},
+	{
+		name: 'IBM Plex Sans Thai',
+		isThai: true
+	},
+	{
+		name: 'IBM Plex Sans Thai Looped',
+		isThai: true
+	},
+	{
+		name: 'Itim',
+		isThai: true
+	},
+	{
+		name: 'Kanit',
+		isThai: true
+	},
+	{
+		name: 'Kodchasan',
+		isThai: true
+	},
+	{
+		name: 'KoHo',
+		isThai: true
+	},
+	{
+		name: 'Krub',
+		isThai: true
+	},
+	{
+		name: 'Kufam',
+		isThai: true
+	},
+	{
+		name: 'Mali',
+		isThai: true
+	},
+	{
+		name: 'Mitr',
+		isThai: true
+	},
+	{
+		name: 'Niramit',
+		isThai: true
+	},
+	{
+		name: 'Pattaya',
+		isThai: true
+	},
+	{
+		name: 'Pridi',
+		isThai: true
+	},
+	{
+		name: 'Prompt',
+		isThai: true
+	},
+	{
+		name: 'Sarabun',
+		isThai: true
+	},
+	{
+		name: 'Srisakdi',
+		isThai: true
+	},
+	{
+		name: 'Taviraj',
+		isThai: true
+	},
+	{
+		name: 'Trirong',
+		isThai: true
+	}
 ];
